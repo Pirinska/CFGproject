7a57fa8c20c0368609668578484ee6edcab63c48
Zuzanna <54065733+marcheweczka007@users.noreply.github.com>
2023-10-27 17:58:35 +0100

    Add array for each topic

--- code.js
+++ code.js
@@ -73,9 +73,16 @@ document.getElementById("startChallengeBtn").addEventListener("click", function(
 });
 
 
-      //Create an array of UX challenges
-      const uxChallenge = ["Mobile Checkout Process: Redesign the mobile checkout process for an e-commerce app to reduce cart abandonment and make it more user-friendly" ,"Accessibility Improvements: Enhance the accessibility of a government website to ensure it's usable by people with disabilities, including screen readers and keyboard navigation.", "Onboarding for a Complex App: Create an intuitive onboarding experience for a complex software application, helping users quickly understand its core features and functionalities.", "Dashboard Customization: Develop a dashboard for a data analytics tool that allows users to customize and personalize their view, prioritizing the most relevant data for their needs.", "E-Commerce Product Filtering: Improve the product filtering and sorting options on an online store to make it easier for customers to find and purchase products." ];
+      //Create an array of UX challenges for e-commerce
+      const uxChallengeEcommerce = ["Mobile Checkout Process: Redesign the mobile checkout process for an e-commerce app to reduce cart abandonment and make it more user-friendly" ,"Accessibility Improvements: Enhance the accessibility of a government website to ensure it's usable by people with disabilities, including screen readers and keyboard navigation.", "Onboarding for a Complex App: Create an intuitive onboarding experience for a complex software application, helping users quickly understand its core features and functionalities.", "Dashboard Customization: Develop a dashboard for a data analytics tool that allows users to customize and personalize their view, prioritizing the most relevant data for their needs.", "E-Commerce Product Filtering: Improve the product filtering and sorting options on an online store to make it easier for customers to find and purchase products." ];
 
-      console.log(uxChallenge[0]);
 
       
+      //Create an array of UX challenges for Health & Fitness
+      const uxChallengeHealth =[];
+
+      //Create an array of UX challenges for Social Media
+      const uxChallengeMedia =[];
+
+      //Create an array of UX challenges for Food
+      const uxChallengeFood =[];
