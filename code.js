ff9cd14a7198077b03e20f33f5dc69c1a4a9afc0
Pirinska <40432179@live.napier.ac.uk>
2023-10-31 20:05:38 +0200

    Small changes to code.js

--- code.js
+++ code.js
@@ -8,7 +8,10 @@ document.getElementById("startChallengeBtn").addEventListener("click", function(
   }
   );
 
-  
+if(document.getElementById) {
+    
+}
+
 
 const uxChallengeEcommerce = ["Mobile Checkout Process: Redesign the mobile checkout process for an e-commerce app to reduce cart abandonment and make it more user-friendly", "Accessibility Improvements: Enhance the accessibility of a government website to ensure it's usable by people with disabilities, including screen readers and keyboard navigation.", "Onboarding for a Complex App: Create an intuitive onboarding experience for a complex software application, helping users quickly understand its core features and functionalities.", "Dashboard Customization: Develop a dashboard for a data analytics tool that allows users to customize and personalize their view, prioritizing the most relevant data for their needs.", "E-Commerce Product Filtering: Improve the product filtering and sorting options on an online store to make it easier for customers to find and purchase products."];
 
